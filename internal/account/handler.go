33d3f2570b2068b98dcf2e71c3338a1c0bd45f73
Anant-raj2 <heyanantraj@gmail.com>
2024-12-26 15:38:13 -0600

    deployment

--- internal/account/handler.go
+++ internal/account/handler.go
@@ -3,5 +3,5 @@ package account
 import "github.com/gofiber/fiber/v2"
 
 func (a *Account) Health(ctx *fiber.Ctx) error {
-	return ctx.JSON("Server: Very Good")
+	return ctx.JSON("Server: Too Good")
 }
